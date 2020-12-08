cdc7f504f8e125c55c1ffa89043014ba7707af30
Menutra <9866630+Menutra@users.noreply.github.com>
2020-12-08 12:03:51 +0000

    Update CheckpointData.h

--- src/CheckpointData.h
+++ src/CheckpointData.h
@@ -18,7 +18,8 @@ namespace CryptoNote {
    * @param CheckpointData Uses data to help sync with the network quicker and to avoid split-chains.
    */
   const std::initializer_list<CheckpointData> CHECKPOINTS = {  
-    //{0, "b9dc432e56e37b52771970ce014dd23fda517cfd4fc5a9b296f1954b7d4505de"},
+    //{0, "f204990ad47bf5cfa8ead24383ebfc99c099bfff446f272cffbc169ab3de1e0d"},
+   {15191, "983ccab3bc1dbd67d2f7caef25571e91f2ab1f3f7fbfb9437033c2c01e1440a1"},  
    {16334, "74ac00598a5e89b5a865919758bbeef3513c6d8a75d4ea315c0cdd7350106809}
        };
 } // namespace CryptoNote
